b3909a9f0cf9f02599529f4e97507a7be23340e6
Yuri Burk <jefferson.burk@ambevtech.com.br>
2024-08-09 19:53:46 -0300

    Change to show movies poster

--- src/utils/mapper.ts
+++ src/utils/mapper.ts
@@ -1,15 +1,17 @@
 import { Movie } from '@/domain/Movie'
 
 export const mapMovies = (movies: Record<string, unknown>[]) =>
-  movies.map(
-    (movie) =>
-      ({
-        id: movie.id,
-        title: movie.title ?? movie.name,
-        originalTitle: movie.original_title ?? movie.original_name,
-        description: movie.overview,
-        image: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
-        date: movie.release_date ?? movie.first_air_date,
-        popularity: movie.vote_average
-      }) as Movie
-  )
+  movies
+    .filter((movie) => movie.poster_path)
+    .map(
+      (movie) =>
+        ({
+          id: movie.id,
+          title: movie.title ?? movie.name,
+          originalTitle: movie.original_title ?? movie.original_name,
+          description: movie.overview,
+          image: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+          date: movie.release_date ?? movie.first_air_date,
+          popularity: movie.vote_average
+        }) as Movie
+    )
